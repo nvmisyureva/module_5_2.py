3c7e8aa7d11aeb30b02a17fec9d1894f11c6eaf9
nvmisyureva <168543591+nvmisyureva@users.noreply.github.com>
2024-10-29 20:25:50 +0500

    Add files via upload

---
+++ module_5_2.py
@@ -0,0 +1,33 @@
+#Задача "Магические здания"
+
+class House:
+    def __init__(self, name, number_of_floors):
+
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+
+    def go_to(self, new_floor):
+        for i in range(1, new_floor + 1):
+            if 1 <= new_floor <= self.number_of_floors:
+                print(i)
+            else:
+                print('Такого этажа не существует')
+                break
+
+    def __len__(self):
+        return self.number_of_floors
+
+
+    def __str__(self):
+        return f"Название: {self.name}, количество этажей: {self.number_of_floors}"
+
+h1 = House('ЖК Эльбрус', 10)
+h2 = House('ЖК Акация', 20)
+
+print(h1)
+print(h2)
+
+print(len(h1))
+print(len(h2))
+
